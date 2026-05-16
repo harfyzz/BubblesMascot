d35f53fe56d3d3882d0a3cab981d8c27f2380b7f
Afeez <harfyzz@gmail.com>
2026-05-16 14:54:16 +0100

    Initial Commit

---
+++ BubblesMascot/BubblesMascotApp.swift
@@ -0,0 +1,17 @@
+//
+//  BubblesMascotApp.swift
+//  BubblesMascot
+//
+//  Created by Afeez Yunus on 16/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct BubblesMascotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BubblesMascot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BubblesMascot
+//
+//  Created by Afeez Yunus on 16/05/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
